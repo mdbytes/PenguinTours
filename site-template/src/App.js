05f811575142332705c667a0253a1e819ee85dde
martindwyer <martin@mdbytes.com>
2021-12-02 17:19:50 -0600

    Getting close to finished

---
+++ site-template/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import logo from "./images/logo.svg";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import "./css/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
