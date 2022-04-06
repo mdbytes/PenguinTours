bfda972303afd9bac54fcb5e70686e10c9f6ecd5
martindwyer <martin@mdbytes.com>
2022-04-06 13:24:55 -0500

    Refactor images to build path

--- site-template-next/components/LandingPage.js
+++ site-template-next/components/LandingPage.js
@@ -3,12 +3,9 @@ import Seo from "./layout/Seo";
 import Script from "next/script";
 import Image from "next/image";
 
-const videoImage =
-  "https://dm2306files.storage.live.com/y4m3tcDl--fSr4DTBJ0Qss5EZmsbpJ_8U-2vkxv_rCDSE8wue9SZKHMhkmWI5r_OZM5avJf6-B3NVWlkFcDl5iVKK65q9S91AOiPMncDGEnb0cUnl-rDb7X1IxEIWKWHe-6dV47VvuMzHdq60agVXsaLP4dhWMR0u14tOiMd9U66idN5mwI6AxJ_Sp-BodWr7gy?width=550&height=400&cropmode=none";
-const nameLogo =
-  "https://dm2306files.storage.live.com/y4mZIW982j-mKInSEhnLWz57VvUv0BFOegVtCMG_3d9mB7HpDrx0p6r3WUmAoakQy-Gyv9SrI656yWkxTq9mBGZy9-4rpAMwXdeTx8FLZhllm0CVcUVAwiQ7zvGHo4MB05Wn0BA3cX-Z3-fJB6wWxjcNbwqm0nl0UbIa5r1_YrGbL7xJ4-pupJpxn2pgISBtQsM?width=1112&height=157&cropmode=none";
-const tagLine =
-  "https://dm2306files.storage.live.com/y4mdTmQtf73acqe1VlzBVKMh_bYxWLxl14bcZlXYjKro2z6aiLHhaQaFDdihMPXHb1Acid6rEzVItwMXOzvgwcKb9dGlI0Y6BN5Vs1MSPP1OyUn2sli60X7CxZAue5NUpBkhP_QAtm8dCXX2LZIWJcInlO185rbGJh5MGTvZ9GhU4s1bAa1Kl3mxoZjFDs05Ytl?width=1029&height=132&cropmode=none";
+const videoImage = "/images/landing-page/video-image-small.jpeg";
+const nameLogo = "/images/landing-page/name-image.png";
+const tagLine = "/images/landing-page/tagline.png";
 
 const LandingPage = () => {
   const [lightbox, setLightbox] = useState(null);
