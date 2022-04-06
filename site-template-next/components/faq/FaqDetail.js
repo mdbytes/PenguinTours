d2d44f28063a62c980c19d3d561a75896abffe52
martindwyer <martin@mdbytes.com>
2022-04-06 13:43:05 -0500

    Update FAQ

--- site-template-next/components/faq/FaqDetail.js
+++ site-template-next/components/faq/FaqDetail.js
@@ -1,6 +1,6 @@
 const faqDetail = [
   {
-    question: "What are the main features",
+    question: "What are the main features?",
     answer:
       "This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the .accordion-body, though the transition does limit overflow.",
     key: 1,
